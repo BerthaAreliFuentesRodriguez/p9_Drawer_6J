4d3a0c157b7498addda7c76aa9392d849af5f07f
BERTHA_ARELI_FUENTES_RODRIGUEZ <a.21308051280465@cbtis128.edu.mx>
2024-02-29 04:28:44 +0000

    drawer actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,126 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiDrawerFuentes());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiDrawerFuentes extends StatelessWidget {
+  const MiDrawerFuentes({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Drawer Fuentes"),
+            centerTitle: true,
+            backgroundColor: Colors.pink,
+          ),
+          backgroundColor: const Color(0xffede7cc),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text("Tienda de comics"),
+                  accountEmail: Text("a.21308051280465@cbtis128.edu.mx"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://raw.githubusercontent.com/BerthaAreliFuentesRodriguez/practica1_6J_IOS/main/p9_drawer/icon.jpeg"),
+                  ),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        "https://raw.githubusercontent.com/BerthaAreliFuentesRodriguez/practica1_6J_IOS/main/p9_drawer/fondo.avif",
+                      ),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  otherAccountsPictures: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/BerthaAreliFuentesRodriguez/practica1_6J_IOS/main/p9_drawer/icon1.jpg"),
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      backgroundImage: NetworkImage(
+                          "https://raw.githubusercontent.com/BerthaAreliFuentesRodriguez/practica1_6J_IOS/main/p9_drawer/icon2.jpg"),
+                    ),
+                  ],
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.account_box_sharp,
+                    color: Color(0xff572c10),
+                  ),
+                  title: Text(
+                    "Usuarios",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.account_tree_sharp,
+                    color: Color(0xff0e3821),
+                  ),
+                  title: Text(
+                    "Empleados",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.add_moderator_sharp,
+                    color: Color(0xff572c10),
+                  ),
+                  title: Text(
+                    "Seguridad y terminos",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.archive_rounded,
+                    color: Color(0xff572c10),
+                  ),
+                  title: Text(
+                    "Productos",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.card_membership_sharp,
+                    color: Color(0xff572c10),
+                  ),
+                  title: Text(
+                    "Ventas",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.car_rental_sharp,
+                    color: Color(0xff572c10),
+                  ),
+                  title: Text(
+                    "Compras",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.note_alt,
+                    color: Color(0xff572c10),
+                  ),
+                  title: Text(
+                    "Cerrar sesion",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
